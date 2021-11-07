f5e39f700f8270e262b7ffe5ca91abfbedb0e763
Nichidanil <nichi.par@mail.ru>
2021-11-07 16:39:23 +0700

    change temp_converter

--- temperature/temp_converter.rb
+++ temperature/temp_converter.rb
@@ -8,21 +8,21 @@ class Converter
     @scale = scale
   end
 
-  def convert_c(scale)
-    @temp += 273.15 if scale[/^[Kk]$/]
-    @temp = (9 * @temp / 5) + 32 if scale[/^[Ff]$/]
+  def convert_c
+    @temp += 273.15 if @scale[/^[Kk]$/]
+    @temp = (9 * @temp / 5) + 32 if @scale[/^[Ff]$/]
     @temp = @temp.round(2)
   end
 
-  def convert_f(scale)
-    @temp = 5 * (@temp - 32) / 9 if scale[/^[Cc]$/]
-    @temp = (5 * (@temp - 32) / 9) + 273.15 if scale[/^[Kk]$/]
+  def convert_f
+    @temp = 5 * (@temp - 32) / 9 if @scale[/^[Cc]$/]
+    @temp = (5 * (@temp - 32) / 9) + 273.15 if @scale[/^[Kk]$/]
     @temp = @temp.round(2)
   end
 
-  def convert_k(scale)
-    @temp -= 273.15 if scale[/^[Cc]$/]
-    @temp = (9 * (@temp - 273.15) / 5) + 32 if scale[/^[Ff]$/]
+  def convert_k
+    @temp -= 273.15 if @scale[/^[Cc]$/]
+    @temp = (9 * (@temp - 273.15) / 5) + 32 if @scale[/^[Ff]$/]
     @temp = @temp.round(2)
   end
 end
